2197db6d62cfc64496a98fe4dc7fa0ae620bc881
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2019-09-25 19:13:13 +0200

    First iteration of multiple tool compiling functionality

--- src/functions/getTools.ts
+++ src/functions/getTools.ts
@@ -1,13 +1,37 @@
 import fs, { PathLike } from "fs";
+import path from 'path';
+var convert = require('xml-js');
 
-function getToolNames(directory:PathLike) : string[] {
-    let directories : string[] = [];
-    fs.readdirSync(directory, { withFileTypes: true })
-        .filter(element => element.isDirectory() && !element.name.startsWith('.'))
-        .forEach(file => {
-            directories.push(file.name)
-        });
-    return directories;
+class ToolManager {
+    static getToolNames(directory:PathLike) : string[] {
+        let directories : string[] = [];
+        fs.readdirSync(directory, { withFileTypes: true })
+            .filter(element => element.isDirectory() && !element.name.startsWith('.'))
+            .forEach(file => {
+                directories.push(file.name)
+            });
+        return directories;
+    }
+    static getToolObject(location: string,toolName: string) : any{
+        try {
+            var toolDir = path.join(location,toolName)
+            var defaultToolPath = "tool/src/webapp/tools"
+            var toolXmlFile = fs.readdirSync(path.join(toolDir,defaultToolPath))[0]
+            var xmlFile = fs.readFileSync(path.join(toolDir,defaultToolPath,toolXmlFile), 'utf8');
+            var result = convert.xml2js(xmlFile, {
+                ignoreComment: true,
+                alwaysChildren: true
+            });
+            // console.log(result);
+            return result
+        } catch (error) {
+            // console.log({ERROR: {
+            //     from: __filename + ' -> ' + 'getToolObject()',
+            //     error
+            // }});
+        }
+    }
 }
 
-export default getToolNames;
+
+export default ToolManager;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import inquirer from 'inquirer';
-import getToolNames from './functions/getTools';
-let tools : string[] = getToolNames('L:\\DESARROLLO\\Sakai\\source\\master');
-console.log(tools);
+const xmlJs = require('xml-js');
+
+import ToolManager from './functions/getTools';
+
+interface SakaiTool{
+    id: string,
+    title: string,
+    description?: string,
+    categories ?: string[]
+}
+
+var toolPath: string = 'L:\\DESARROLLO\\Sakai\\source\\master'
+let tools : string[] = ToolManager.getToolNames(toolPath);
 
 async function app() {
     var promptData = await inquirer.prompt(
@@ -12,7 +22,38 @@ async function app() {
             choices: tools
           }
     )
-    console.log({promptData});
+    // var tool = ToolManager.getToolObject(toolPath, promptData.selectedTools[0])
+    // var auxAttributes = tool.elements[0].elements[0].attributes
+    // var testTool : SakaiTool = {
+    //     id: auxAttributes.id,
+    //     title: auxAttributes.title,
+    //     description: auxAttributes.description,
+    //     categories: tool.elements[0].elements[0].elements.map(el => el.attributes.name )
+    // }
+    // console.log(testTool);
+    var listOfToolsWithValidXml : SakaiTool[] = []
+    promptData.selectedTools.forEach((el, index) => {
+        try {
+            var tool = ToolManager.getToolObject(toolPath, el)
+            var auxAttributes = tool.elements[0].elements[0].attributes
+            var testTool : SakaiTool = {
+                id: auxAttributes.id,
+                title: auxAttributes.title,
+                description: auxAttributes.description,
+                categories: tool.elements[0].elements[0].elements.map(el => el.attributes.name )
+            }
+            // console.log(`Tool #${index}:`);
+            // console.log(testTool);
+            listOfToolsWithValidXml.push(testTool)
+        } catch (error) {
+            // console.log('shit happens :'+el);
+        }
+    });
+    console.log(listOfToolsWithValidXml);
+    // console.log(tool.elements);
+    // console.log(tool.elements[0].elements[0].attributes);
+    // console.log(tool.elements[0].elements[0].elements.map(el => el.attributes.name )); // Categories
+    
 }
 
 
