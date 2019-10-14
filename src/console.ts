0e21126a43050c06cf6619401674fdf881f7d69e
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2019-10-14 19:35:27 +0200

    CODE REOGANIZE
    - Move frontend to root folder
    - Move old root to console.ts
    - Pending rework relative paths

---
+++ src/console.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import path from 'path';
+import ToolManager from './functions/ToolManager';
+const consola = require('consola')
+
+async function app() {
+    let sakaiPath = process.cwd()
+
+    let promptData = await inquirer.prompt(
+        {
+            type: "checkbox",
+            name: 'selectedTools',
+            message: 'Which tools do you want to compile?',
+            choices: ToolManager.getToolNames(sakaiPath)
+        }
+    )
+    let confirmation = await inquirer.prompt(
+        {
+            type: "confirm",
+            name: 'value',
+            message: `Compile this tools to '${process.env.CATALINA_BASE}' ? \n-${promptData.selectedTools.join('\n-')}\n`,
+            default: true
+        }
+    )
+    if(!confirmation.value){
+        return
+    }
+    let errors: string[] = [];
+    for (const toolName of promptData.selectedTools) {
+        let success = await ToolManager.deployTool(toolName,sakaiPath)
+        if(!success){
+            errors.push(toolName)
+            consola.warn('-Skipping tool : '+toolName);
+        }
+    }
+    let successTotal = promptData.selectedTools.length - errors.length
+    consola.info(`-- Deployed ${successTotal}/${promptData.selectedTools.length} tools --`)
+    if(errors.length > 0){
+        consola.warn('Failed to deploy :\n'+errors.map(el => '- '+el).join('\n'))
+    }
+}
+
+
+app()
--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,18 @@
-#!/usr/bin/env node
-import inquirer from 'inquirer';
-import path from 'path';
-import ToolManager from './functions/ToolManager';
-const consola = require('consola')
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-async function app() {
-    let sakaiPath = process.cwd()
+Vue.config.productionTip = false;
 
-    let promptData = await inquirer.prompt(
-        {
-            type: "checkbox",
-            name: 'selectedTools',
-            message: 'Which tools do you want to compile?',
-            choices: ToolManager.getToolNames(sakaiPath)
-        }
-    )
-    let confirmation = await inquirer.prompt(
-        {
-            type: "confirm",
-            name: 'value',
-            message: `Compile this tools to '${process.env.CATALINA_BASE}' ? \n-${promptData.selectedTools.join('\n-')}\n`,
-            default: true
-        }
-    )
-    if(!confirmation.value){
-        return
-    }
-    let errors: string[] = [];
-    for (const toolName of promptData.selectedTools) {
-        let success = await ToolManager.deployTool(toolName,sakaiPath)
-        if(!success){
-            errors.push(toolName)
-            consola.warn('-Skipping tool : '+toolName);
-        }
-    }
-    let successTotal = promptData.selectedTools.length - errors.length
-    consola.info(`-- Deployed ${successTotal}/${promptData.selectedTools.length} tools --`)
-    if(errors.length > 0){
-        consola.warn('Failed to deploy :\n'+errors.map(el => '- '+el).join('\n'))
-    }
-}
-
-
-app()
+new Vue({
+  router,
+  store,
+  // @ts-ignore
+  vuetify,
+  render: h => h(App),
+}).$mount('#app');
