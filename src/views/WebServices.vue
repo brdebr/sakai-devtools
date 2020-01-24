c7a4e3a872ab0d2560a1f3fa4a070574b672a3d2
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-01-24 22:01:12 +0100

    add transition

--- src/views/WebServices.vue
+++ src/views/WebServices.vue
@@ -1,30 +1,32 @@
 <template>
-  <v-card dark class="blue darken-2 mx-auto" min-width="100%">
+  <v-card dark class="blue darken-2 mx-auto overflow-hidden" min-width="100%" min-height="500px">
     <v-card-title class="elevation-2 blue darken-3 pt-3">
       WebServices
     </v-card-title>
-    <v-card-text class="pt-5 pb-3" v-if="sessionId">
-      <v-row no-gutters wrap align="center">
-        <v-col no-gutters class="flex-grow-1 flex-shrink-0">
-          <v-row no-gutters>
-            <v-col no-gutters cols="12">
-              Logged In
-            </v-col>
-            <v-col no-gutters cols="12">
-              {{ sessionId }}
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col no-gutters class="flex-grow-0">
-          <v-btn depressed @click="logOut">
-            Log out
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card-text>
-    <v-card-text class="pt-5 pb-3" v-else>
-      <LoginForm @logged="logged" />
-    </v-card-text>
+    <transition :name="`move-x${!sessionId ? '-reverse' :''}`" mode="out-in">
+      <v-card-text class="pt-5 pb-3" v-if="sessionId" key="in">
+        <v-row no-gutters wrap align="center">
+          <v-col no-gutters class="flex-grow-1 flex-shrink-0">
+            <v-row no-gutters>
+              <v-col no-gutters cols="12">
+                Logged In:
+              </v-col>
+              <v-col no-gutters cols="12">
+                {{ sessionId }}
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col no-gutters class="flex-grow-0">
+            <v-btn depressed @click="logOut">
+              Log out
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-text class="pt-5 pb-3" v-else key="out">
+        <LoginForm @logged="logged" />
+      </v-card-text>
+    </transition>
   </v-card>
 </template>
 
