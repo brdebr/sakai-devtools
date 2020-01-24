a41ba72ce5ca5b6ba5cdc6bef7ed4b36857c1299
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-01-24 20:07:49 +0100

    add simple template

---
+++ utils/vue-ts-component.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class TEMPLATE_CLASS extends Vue {
+
+  @Prop({ default: false })
+  disabled!: boolean;
+
+  value: Array<Number> = [];
+
+  @Watch("value")
+  logLen(val: Array<Number>, old: Array<Number>) {
+    console.log(val.length);
+  }
+  get negateDisable() {
+    return !this.disabled
+  }
+
+}
+</script>
