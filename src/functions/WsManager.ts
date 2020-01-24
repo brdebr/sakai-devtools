7baf7293731f9822ab4bf0b5a2ffe1d1bc0811b4
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2020-01-24 22:59:29 +0100

    improve ws manager

--- src/functions/WsManager.ts
+++ src/functions/WsManager.ts
@@ -1,19 +1,56 @@
-const axios = require('axios');
-
 export interface LoginParams {
-    id: String,
-    pw: String
+  id: String;
+  pw: String;
 }
 
+export interface sakaiParam {
+  sessionid: String;
+}
+
+export interface addUserParams extends sakaiParam {
+  eid: String;
+  firstname: String;
+  lastname: String;
+  email: String;
+  type: String;
+  password: String;
+}
+
+const axios = require("axios");
+const restEndpoint = "/sakai-ws/rest";
+const headers = {
+  Accept: "text-plain"
+};
+
 export default class WebServiceManager {
-    static async login(params: LoginParams): Promise<String>{
-        let { data } = await axios.get('http://localhost:8080/sakai-ws/rest/login/login', {
-            headers: {
-                'Accept': 'text-plain'
-            },
-            withCredentials: false,
-            params
-        })
-        return data
-    }
+  static async login(params: LoginParams, baseURL: String): Promise<String> {
+    let endpoint = "/login/login";
+    let { data } = await axios.get(baseURL + restEndpoint + endpoint, {
+      headers,
+      params
+    });
+    return data;
+  }
+  static async resetAllUserWorkspace(
+    params: sakaiParam,
+    baseURL: String
+  ): Promise<Boolean> {
+    let endpoint = "/sakai/resetAllUserWorkspace";
+    let { data } = await axios.get(baseURL + restEndpoint + endpoint, {
+      headers,
+      params
+    });
+    return data;
+  }
+  static async addNewUser(
+    params: addUserParams,
+    baseURL: String
+  ): Promise<String> {
+    let endpoint = "/sakai/addNewUser";
+    let { data } = await axios.get(baseURL + restEndpoint + endpoint, {
+      headers,
+      params
+    });
+    return data;
+  }
 }
